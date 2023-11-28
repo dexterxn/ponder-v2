f9b898e0e9b5afa7ed6a5a00b84cb4d9783a81ea
dexterxn <dexterxn@gmail.com>
2023-11-28 11:11:19 -0800

    initial commit

---
+++ src/app.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+mongoose.set('strictQuery', false);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config();
+}
+
+const PORT = process.env.PORT || 3000;
+const CONNECTION = process.env.CONNECTION;
+
+const customers = [
+    {
+        "name": "Caleb",
+        "industry": "music"
+    },
+    {
+        "name": "John",
+        "industry": "networking"
+    },
+    {
+        "name": "Sal",
+        "industry": "sports medicine"
+    }
+];
+
+app.get('/', (req, res) => {
+    res.send('welcome');
+});
+
+app.get('/api/customers', (req, res) => {
+    res.send({"customers": customers});
+});
+
+app.post('/api/customers', (req, res) => {
+    console.log(req.body);
+    console.log('sending api customer post request');
+    res.send(req.body);
+});
+
+app.post('/', (req, res) =>{
+    res.send('This is a post request!');
+});
+
+const start = async() => {
+    try{
+        await mongoose.connect(CONNECTION);
+
+        app.listen(PORT, () => {
+            console.log('App listening on port ' + PORT);
+        });
+    } catch(e) {
+        console.log(e.message)
+    }
+};
+
+start();
