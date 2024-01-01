099697887916de12cbbb1a32db094b14a828a9c4
dexterxn <dexterxn@gmail.com>
2024-01-01 10:32:15 -0800

    Added typscript files

---
+++ src/app.ts
@@ -0,0 +1,161 @@
+//@ts-nocheck
+
+const express = require('express');
+const mongoose = require('mongoose');
+const Customer = require('./models/customer');
+const cors = require('cors');
+
+const app = express();
+mongoose.set('strictQuery', false);
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config();
+}
+
+const PORT = process.env.PORT || 3005;
+const CONNECTION = process.env.CONNECTION;
+
+
+app.get('/', (req, res) => {
+    res.send("Welcome!!");
+});
+
+app.get('/api/customers', async (req, res) => {
+    try{
+        const result = await Customer.find();
+        res.send({"customers": result});
+    } catch (e){
+        res.status(500).json({error: e.message});
+    }
+    
+});
+
+app.get('/api/customers/:id',async(req, res) => {
+    console.log({
+        requestParams: req.params,
+        requestQuery: req.query
+    });
+    try{
+        const customerId = req.params.id;
+        console.log(customerId);
+        const customer = await Customer.findById(customerId);
+        console.log(customer);
+        if(!customer){ // if customer == null 
+            res.status(404).json({error: 'user not found'});
+        }else{
+            res.json({customer});
+        }
+    } catch (e){
+        res.status(500).json({error: 'something went wrong'});
+
+    }
+    
+});
+
+app.put('/api/customers/:id', async(req,res)=>{
+    try{
+        const customerId = req.params.id;
+        const customer = await Customer.findOneAndReplace({_id: customerId}, req.body, {new: true});
+        console.log(customer);
+        res.json({customer});
+    } catch(e) {
+        res.status(500).json({error: 'something went wrong'});
+    }
+    
+});
+
+app.patch('/api/customers/:id', async(req,res)=>{
+    try{
+        const customerId = req.params.id;
+        const customer = await Customer.findOneAndUpdate({_id: customerId}, req.body, {new: true});
+        console.log(customer);
+        res.json({customer});
+    } catch(e) {
+        res.status(500).json({error: 'something went wrong'});
+    }
+    
+});
+
+app.patch('/api/orders/:id', async(req,res)=>{
+    console.log(req.params);
+    const orderId = req.params.id;
+    req.body._id = orderId;
+    try{
+        const result = await Customer.findOneAndUpdate(
+            {'orders._id' : orderId }, 
+            {$set: {'orders.$': req.body}},
+            {new: true}
+            );
+
+        console.log(result);
+        if(result){
+            res.json(result);
+        }else{
+            res.status(404).json({error: 'Order not found'});
+        }
+    } catch(e) {
+        console.log(e.message);
+        res.status(500).json({error: 'something went wrong'});
+    }
+    
+});
+
+app.get('/api/orders/:id', async(req,res)=>{
+    try{
+        const result = await Customer.findOne({'orders._id': req.params.id});
+        if(result){
+            res.json(result);
+        }else{
+            res.status(404).json({error: 'Order not found'});
+        }
+    } catch (e){
+        console.log(e);
+        res.status(500).json({error: 'something went wrong'});
+
+    }
+});
+
+
+app.delete('/api/customers/:id', async(req,res)=>{
+    try{
+        const customerId = req.params.id;
+        const result = await Customer.deleteOne({_id: customerId});
+        res.json({deletedCount: result.deletedCount});
+    }catch(e){
+        res.status(500).json({error: 'something went wrong'});
+    }   
+});
+
+app.post('/api/customers', async (req, res) => {
+    console.log(req.body);
+    const customer = new Customer(req.body);
+    try{
+        await customer.save();
+        res.status(201).json({customer});
+    }catch(e){
+        res.status(400).json({error: e.message});
+    }
+    
+});
+
+app.post('/', (req, res) =>{
+    res.send('This is a post request!');
+});
+
+const start = async() => {
+    try{
+        await mongoose.connect(CONNECTION);
+
+        app.listen(PORT, () => {
+            console.log('App listening on port ' + PORT);
+        });
+    } catch(e) {
+        console.log(e.message)
+    }
+};
+
+start();
